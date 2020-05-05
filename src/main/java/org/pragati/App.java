3b2fa65e2103ca9ece99a0a169bacf57ecb78289
pragati <pragatigupta989@gmail.com>
2020-05-05 16:44:33 +0530

    Initial commit

---
+++ src/main/java/org/pragati/App.java
@@ -0,0 +1,13 @@
+package org.pragati;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
