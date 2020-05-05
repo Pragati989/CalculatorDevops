2012287a66e2df5258df842df6854569288e679b
pragati <pragatigupta989@gmail.com>
2020-05-05 17:21:03 +0530

    add/diff added

---
+++ src/main/java/org/pragati/Calculator.java
@@ -0,0 +1,55 @@
+package org.pragati;
+import java.util.Scanner;
+public class Calculator {
+
+    public static int add(int a,int b) { return (a+b); }
+    public static int diff(int a,int b) { return (a-b); }
+
+    public static void main(String args[])
+    {
+        int choice,result;
+        int value1,value2;
+        Scanner sc=new Scanner(System.in);
+
+        while(true)
+        {
+            System.out.println("------MENU------");
+            System.out.println("1. Addition \n"+
+                               "2. Subtraction \n"+
+                               "3. Multiplication \n"+
+                               "4. Division \n"+
+                               "5. Exit \n");
+            System.out.println("Please enter your choice :");
+            choice=sc.nextInt();
+
+                switch(choice)
+                {
+                    case 1: System.out.println("Enter the first number");
+                            value1=sc.nextInt();
+                            System.out.println("Enter the second number");
+                            value2=sc.nextInt();
+                            result=add(value1,value2);
+                            System.out.println(result);
+                            break;
+                    case 2: System.out.println("Enter the first number");
+                            value1=sc.nextInt();
+                            System.out.println("Enter the second number");
+                            value2=sc.nextInt();
+                            result=diff(value1,value2);
+                            System.out.println(result);
+                            break;
+                    case 5: System.out.println("Terminating...PLease wait!!");
+                            System.exit(0);
+                            break;
+
+                    default:System.out.println("Enter a valid choice!");
+                            break;
+
+
+                }//switch ends
+
+        }//while ends
+        // sc.close();
+
+    }
+}
